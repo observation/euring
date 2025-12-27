dee09f2e02dc2ffc6bbe6a1574d54c3aca7b27c0
Dylan Verheul <dylan@dyve.net>
2025-12-27 16:03:46 +0100

    Format

--- src/euring/codes.py
+++ src/euring/codes.py
@@ -3,12 +3,12 @@ from datetime import date
 from .data import (
     load_code_map,
     load_other_marks_data,
-    load_place_map,
     load_place_details,
-    load_scheme_map,
+    load_place_map,
     load_scheme_details,
-    load_species_map,
+    load_scheme_map,
     load_species_details,
+    load_species_map,
 )
 from .exceptions import EuringParseException
 from .utils import euring_dms_to_float
