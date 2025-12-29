c9adf9125b3d4286840ac7f936c3e906fed0ecd9
Dylan Verheul <dylan@dyve.net>
2025-12-29 13:23:37 +0100

    Move test data to Python fixtures

---
+++ tests/fixtures/euring2000_examples.py
@@ -0,0 +1,11 @@
+"""EURING2000 example records with provenance for tests."""
+
+EURING2000_EXAMPLES = [
+    # Source:
+    # Speek, G., Clark, J.A., Rohde, Z., Wassenaar, R.D. & Van Noordwijk, A.J. 2001. The EURING
+    # exchange-code 2000. Heteren. ISBN 90-74638-13-9."
+    "DERAOCD...5206501ZZ1877018770NOZUFE22U-----081019710----DECK+502400+0074200082003----",
+    "DERAOCD...5206514ZZ1877018770NOZUPF02U-----120719760----SV55+584200+01348000101030100202301739",
+]
+
+__all__ = ["EURING2000_EXAMPLES"]
---
+++ tests/fixtures/euring2000plus_examples.py
@@ -0,0 +1,5 @@
+"""EURING2000+ example records with provenance for tests."""
+
+EURING2000PLUS_EXAMPLES = []
+
+__all__ = ["EURING2000PLUS_EXAMPLES"]
