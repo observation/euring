a91b47e5767cb4969cb6745d85c8b0c4e4781e1b
Dylan Verheul <dylan@dyve.net>
2025-12-30 06:54:25 +0100

    Move code tables for Condition and EURING Code Identifier to data

---
+++ src/euring/data/code_table_euring_code_identifier.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+# Manual EURING code table.
+TABLE = [
+    {"code": "0", "description": "EURING Code Manual (1966). Directly coded (no translation)."},
+    {
+        "code": "1",
+        "description": "Code Manual New EURING (1979), but translated from EURING Code Manual (1966).",
+    },
+    {"code": "2", "description": "Code Manual New EURING (1979). Directly coded (no translation of older codes)."},
+    {"code": "3", "description": "Translated from earlier code but exact route uncertain"},
+    {"code": "4", "description": "EURING exchange-code 2000 or 2000+, Directly coded (no translation of older codes)"},
+]
